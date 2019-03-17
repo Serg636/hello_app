7cb7678136b06b5fa97abf81b5dd080337a6cc4b
Serg636 <s.v.sharafanovich@gmail.com>
2019-03-17 21:10:01 +0300

    commit #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+protect_from_forgery with: :exception
+
+  def hello
+  	#render text: "hello, world111!"
+    render html: "hello, world222!"
+  end
 end
